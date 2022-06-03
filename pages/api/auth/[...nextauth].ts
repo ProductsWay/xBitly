9f927122536466aa9d87b4603b62c59d25e11ca3
Huynh Duc Dung <dunghd.it@gmail.com>
2022-06-03 23:07:47 +0800

    :sparkles: add login with github

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,25 +1,11 @@
-import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
-import Adapters from 'next-auth/adapters';
 import Providers from 'next-auth/providers';
-
-const prisma = new PrismaClient();
 
 export default NextAuth({
     providers: [
-        Providers.Email({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
+        Providers.GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
     ],
-    database: process.env.DATABASE_URL,
-    adapter: Adapters.Prisma.Adapter({
-        prisma,
-        modelMapping: {
-            User: 'user',
-            Account: 'account',
-            Session: 'session',
-            VerificationRequest: 'verificationRequest',
-        },
-    }),
 });
