8c3c1bf359662b4cec88e78cb2ed61a5f13f33ba
Huynh Duc Dung <dunghd.it@gmail.com>
2022-06-04 01:44:36 +0800

    :sparkles: add shorted link and save to api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/client';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
 import * as yup from 'yup';
 
 import Layout from '../components/Layout';
@@ -18,12 +20,19 @@ export default function Index() {
     const {
         register,
         handleSubmit,
-        control,
         formState: { errors },
     } = useForm<FormValue>({
         resolver: yupResolver(schema),
     });
-    const onSubmit = (data) => console.log(data);
+
+    const mutation = useMutation((url: string) => {
+        return fetch(process.env.API_URL, {
+            method: 'POST',
+            body: JSON.stringify({ url }),
+        }).then((resp) => resp.json());
+    });
+
+    const onSubmit = (data: FormValue) => mutation.mutate(data.url);
     return (
         <Layout>
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -67,13 +76,98 @@ export default function Index() {
                                 className="w-full max-w-sm mt-2 input"
                             />
 
-                            <button className="mt-2 btn btn-primary">
-                                Create A Short Link
+                            <button
+                                type="submit"
+                                className={
+                                    mutation.isLoading
+                                        ? 'mt-2 btn btn-primary loading'
+                                        : 'mt-2 btn btn-primary'
+                                }
+                            >
+                                {mutation.isLoading
+                                    ? 'Creating A Short Link'
+                                    : 'Create A Short Link'}
                             </button>
+
+                            {mutation.isError ? (
+                                <div>
+                                    An error occurred:{' '}
+                                    {(mutation.error as Error).message}
+                                </div>
+                            ) : null}
+
+                            {!mutation.isLoading && mutation.isSuccess && (
+                                <label
+                                    htmlFor="my-modal"
+                                    className="ml-2 btn modal-button"
+                                >
+                                    Your Link
+                                </label>
+                            )}
                         </div>
                     </div>
                 </div>
             </form>
+
+            {mutation.isSuccess ? (
+                <>
+                    <input
+                        type="checkbox"
+                        id="my-modal"
+                        className="modal-toggle"
+                    />
+                    <div className="modal">
+                        <div className="relative modal-box">
+                            <label
+                                htmlFor="my-modal"
+                                className="absolute btn btn-sm btn-circle right-2 top-2"
+                            >
+                                ✕
+                            </label>
+                            <h3 className="text-lg font-bold">
+                                Congratulations!
+                            </h3>
+
+                            <div className="py-8 form-control">
+                                <div className="justify-center input-group">
+                                    <input
+                                        type="text"
+                                        placeholder="Search…"
+                                        disabled
+                                        className="input input-bordered"
+                                        value={`${window.location.href}${mutation.data?.ID}`}
+                                    />
+                                    <div
+                                        className="tooltip"
+                                        data-tip="Copy to clipboard"
+                                    >
+                                        <CopyToClipboard
+                                            text={`${window.location.href}${mutation.data?.ID}`}
+                                        >
+                                            <button className="btn btn-square">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="w-6 h-6"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor"
+                                                    strokeWidth="2"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </CopyToClipboard>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : null}
         </Layout>
     );
 }
