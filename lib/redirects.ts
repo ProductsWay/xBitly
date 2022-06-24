eed880a4e971beb87d84156fb4b386aac156a440
Huynh Duc Dung <dunghd.it@gmail.com>
2022-06-24 21:00:39 +0800

    :zap: add cache control before redirect

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -1,6 +1,9 @@
+import logger from 'loglevel';
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextRequest, NextResponse } from 'next/server';
+import QuickLRU from 'quick-lru';
 
+const lru = new QuickLRU<string, { URL: string }>({ maxSize: 1000 });
 const apiUrl =
     process.env.API_URL ?? 'https://staging-xbitly-vz82.encr.app/url';
 
@@ -13,10 +16,23 @@ export default async function redirects(req: NextRequest) {
         !url.pathname.includes('api')
     ) {
         const redirectUrl = apiUrl + url.pathname;
+        if (lru.has(redirectUrl)) {
+            logger.info('Found in cache', redirectUrl);
+            const data = lru.get(redirectUrl);
+            if (data.URL) {
+                return NextResponse.redirect(data.URL);
+            }
+        }
+
         const result = await fetch(redirectUrl);
         if (result.ok) {
             const data = await result.json();
-            return NextResponse.redirect(data.URL);
+            lru.set(redirectUrl, data);
+            const res = NextResponse.redirect(data.URL);
+            // add cache control, refer https://vercel.com/docs/concepts/functions/serverless-functions/edge-caching
+            res.headers.set('Cache-Control', 'max-age=0, s-maxage=86400');
+
+            return res;
         }
     }
 }
