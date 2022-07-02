28761e7088cbbd1ac6fe3bd4e15b9b5929f653cb
Huynh Duc Dung <dunghd.it@gmail.com>
2022-07-03 01:09:50 +0800

    chore(next): enable experimental-edge runtime

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { createSecureHeaders } = require('next-secure-headers');
 
 module.exports = {
+    experimental: {
+        // https://nextjs.org/blog/next-12-2#edge-server-rendering-experimental
+        runtime: 'experimental-edge',
+    },
     async headers() {
         return [{ source: '/(.*)', headers: createSecureHeaders() }];
     },
