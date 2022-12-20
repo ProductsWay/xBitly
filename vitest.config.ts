a7303b0614373ac076d35665558df0f73162e38e
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-21 00:00:49 +0800

    chore(deps): update packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // @ts-expect-error
     plugins: [react()],
     test: {
         globals: true,
