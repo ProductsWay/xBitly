0a46df19d2ad327978889aef136c178e9013b554
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-30 22:55:30 +0800

    feat: add goat counter for analysis

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,17 +1,22 @@
-import Head from 'next/head';
+import Script from 'next/script';
 
 import Footer from '../Footer';
 import Header from '../Header';
 
 const Layout: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => (
     <div>
-        <Head>
-            <script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5337133458846513"
-                crossOrigin="anonymous"
-            ></script>
-        </Head>
+        <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5337133458846513"
+            crossOrigin="anonymous"
+        />
+
+        <Script
+            data-goatcounter="https://xbitly.goatcounter.com/count"
+            async
+            src="//gc.zgo.at/count.js"
+        />
+
         <Header links={[]} />
 
         {children}
