1dc54133aa8afbcb9e4cf5af0612d75fb6e42f74
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-30 22:55:09 +0800

    feat: support redirect to threadify

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -6,8 +6,24 @@ const lru = new QuickLRU<string, { URL: string }>({ maxSize: 1000 });
 const apiUrl =
     process.env.API_URL ?? 'https://staging-xbitly-vz82.encr.app/url';
 
+function redirectToThreadify(twitterUrl: string) {
+    const id = twitterUrl.match(/status\/(\d+)/)[1];
+    const url = `https://threadify.productsway.com/thread/${id}`;
+    return NextResponse.redirect(url);
+}
+
+function isTwitterUrl(url: string) {
+    // Use a regular expression to test the URL
+    const pattern = /^https?:\/\/[^/]+\/[a-zA-Z0-9_]+\/status\/[^/]+/;
+    return pattern.test(url);
+}
+
 export default async function redirects(req: NextRequest) {
     const url = req.nextUrl.clone();
+
+    if (isTwitterUrl(url.toString())) {
+        return redirectToThreadify(url.toString());
+    }
 
     if (
         url.pathname.length === 9 &&
