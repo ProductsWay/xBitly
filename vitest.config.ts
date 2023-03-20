ba76b0a40b83c110ea7b72b085be589c78f29c56
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-20 22:06:23 +0700

    chore(deps): update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // @ts-expect-error build error with vitejs
     plugins: [react()],
     test: {
         globals: true,
