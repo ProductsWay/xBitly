22aca2483164a5c4f0ba0c4513790b9cb1ddff40
Huynh Duc Dung <dunghd.it@gmail.com>
2024-05-26 16:25:41 +0800

    refactor!: migrate to msw v2

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,33 +1,29 @@
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 const handlers = [
-    rest.post('/login', (_req, res, ctx) => {
+    http.post('/login', () => {
         // Persist user's authentication in the session
         sessionStorage.setItem('is-authenticated', 'true');
-        return res(
-            // Respond with a 200 status code
-            ctx.status(200),
-        );
+        return HttpResponse.json();
     }),
-    rest.get('/user', (_req, res, ctx) => {
+    http.get('/user', () => {
         // Check if the user is authenticated in this session
         const isAuthenticated = sessionStorage.getItem('is-authenticated');
         if (!isAuthenticated) {
             // If not authenticated, respond with a 403 error
-            return res(
-                ctx.status(403),
-                ctx.json({
+            return HttpResponse.json(
+                {
                     errorMessage: 'Not authorized',
-                }),
+                },
+                {
+                    status: 403,
+                },
             );
         }
         // If authenticated, return a mocked user details
-        return res(
-            ctx.status(200),
-            ctx.json({
-                username: 'admin',
-            }),
-        );
+        return HttpResponse.json({
+            username: 'admin',
+        });
     }),
 ];
 export default handlers;
